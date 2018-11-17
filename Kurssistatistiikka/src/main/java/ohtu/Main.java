4d23608314ccebe1623d69dc70741823197f2324
Susanna Rajamäki <strajama@gmail.com>
2018-11-17 12:14:46 +0200

    eka tehtävä

--- Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -1,7 +1,37 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.fluent.Request;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // ÄLÄ laita githubiin omaa opiskelijanumeroasi
+        String studentNr = "012345678";
+        int te = 0;
+        int tu = 0;
+        if (args.length > 0) {
+            studentNr = args[0];
+        }
+
+        String url = "https://studies.cs.helsinki.fi/courses/students/" + studentNr + "/submissions";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+        System.out.println("json-muotoinen data:");
+        System.out.println(bodyText);
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+
+        System.out.println("opiskelijanumero " + studentNr + "\n");
+        for (Submission submission : subs) {
+            System.out.println(submission);
+            te += submission.getExercises().length;
+            tu += submission.getHours();
+        }
+        System.out.println("\nyhteensä: " + te + " tehtävää ja " + tu
+                + " tuntia");
     }
 }
--- Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -1,7 +1,45 @@
 package ohtu;
 
 public class Submission {
+
     private int week;
+    private int hours;
+    private String course;
+    private int[] exercises;
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int[] getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(int[] exercises) {
+        this.exercises = exercises;
+    }
+
+    public String printExercises() {
+        StringBuilder s = new StringBuilder();
+        for (int e : exercises) {
+            s.append(e);
+            s.append(", ");
+        }
+        s.delete(s.lastIndexOf(","), s.length());
+        return s.toString();
+    }
 
     public void setWeek(int week) {
         this.week = week;
@@ -13,7 +51,10 @@ public class Submission {
 
     @Override
     public String toString() {
-        return ""+week;
+        return course + ", viikko " + week + " tehtyjä tehtäviä yhteensä "
+                + exercises.length + " aikaa kului " + hours + " tehdyt tehtävät: "
+                + printExercises();
     }
-    
+
 }
+// rails2018, viikko 6 tehtyjä tehtäviä yhteensä 2 aikaa kului 2 tehdyt tehtävät: 1, 4
