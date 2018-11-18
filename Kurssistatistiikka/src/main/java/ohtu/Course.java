50bb215c781ec64e7f4cd42354d560f72dd92349
Susanna Rajamäki <strajama@gmail.com>
2018-11-18 17:48:09 +0200

    tehtäviä

---
+++ Kurssistatistiikka/src/main/java/ohtu/Course.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu;
+
+/**
+ *
+ * @author strajama
+ */
+public class Course {
+
+    private String name;
+    private int week;
+    private String term;
+    private int year;
+    private String fullName;
+    private int[] exercises;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int[] getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(int[] exercises) {
+        this.exercises = exercises;
+    }
+
+    public String printName() {
+        return fullName + " " + term + " " + year;
+    }
+
+    public int countExercises() {
+        int a = 0;
+        for (int e: exercises) {
+            a += e;
+        }
+        return a;
+    }
+
+}
+
--- Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -1,37 +1,94 @@
 package ohtu;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import java.io.IOException;
 import org.apache.http.client.fluent.Request;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         // ÄLÄ laita githubiin omaa opiskelijanumeroasi
         String studentNr = "012345678";
-        int te = 0;
-        int tu = 0;
+
         if (args.length > 0) {
             studentNr = args[0];
         }
 
-        String url = "https://studies.cs.helsinki.fi/courses/students/" + studentNr + "/submissions";
+        String subUrl = "https://studies.cs.helsinki.fi/courses/students/" + studentNr + "/submissions";
+        String subBodytext = Request.Get(subUrl).execute().returnContent().asString();
 
-        String bodyText = Request.Get(url).execute().returnContent().asString();
+        String courseUrl = "https://studies.cs.helsinki.fi/courses/courseinfo";
+        String courseBodyText = Request.Get(courseUrl).execute().returnContent().asString();
 
-        System.out.println("json-muotoinen data:");
-        System.out.println(bodyText);
+//        System.out.println("json-muotoinen data:");
+  //      System.out.println(subBodytext);
+    //    System.out.println(courseBodyText);
 
         Gson mapper = new Gson();
-        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+        Submission[] subs = mapper.fromJson(subBodytext, Submission[].class);
+        Course[] courses = mapper.fromJson(courseBodyText, Course[].class);
 
-        System.out.println("opiskelijanumero " + studentNr + "\n");
-        for (Submission submission : subs) {
-            System.out.println(submission);
-            te += submission.getExercises().length;
-            tu += submission.getHours();
+        System.out.println("\nopiskelijanumero " + studentNr + "\n");
+        for (Course course : courses) {
+            boolean print = false;
+            int te = 0;
+            int tu = 0;
+
+            for (Submission submission : subs) {
+                if (submission.getCourse().equals(course.getName())) {
+                    if (!print) {
+                        System.out.println(course.printName());
+                        print = true;
+                    }
+                    System.out.println("viikko " + submission.getWeek() + ":");
+                    System.out.println(" tehtyjä tehtäviä " + submission.getExercises().length
+                            + "/" + course.getExercises()[submission.getWeek()]
+                            + " aikaa kului " + submission.getHours() + " tehdyt tehtävät: " + submission.printExercises());
+                    te += submission.getExercises().length;
+                    tu += submission.getHours();
+                }
+            }
+            if (te + tu != 0) {
+                System.out.println("\nyhteensä: " + te + "/" + course.countExercises() + " tehtävää ja " + tu
+                        + " tuntia\n");
+                try {
+                    String statsResponse = "https://studies.cs.helsinki.fi/courses/" + course.getName() + "/stats";
+                    JsonParser parser = new JsonParser();
+    //                JsonObject parsittuData = parser.parse(statsResponse).getAsJsonObject();
+
+                } catch (JsonSyntaxException e) {
+                    System.out.println("napattu\n");
+                };
+            }
+
         }
-        System.out.println("\nyhteensä: " + te + " tehtävää ja " + tu
-                + " tuntia");
+
     }
 }
+/*
+opiskelijanumero 012345678
+
+Web-palvelinohjelmointi Ruby on Rails syksy 2018
+ 
+viikko 1:
+ tehtyjä tehtäviä 10/11 aikaa kului 10 tehdyt tehtävät: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+viikko 6:
+ tehtyjä tehtäviä 2/15 aikaa kului 2 tehdyt tehtävät: 1, 4
+
+yhteensä: 12/103 tehtävää 12 tuntia
+ 
+Ohjelmistotuotanto syksy 2018
+ 
+viikko 1:
+ tehtyjä tehtäviä 15/17 aikaa kului 20 tehdyt tehtävät: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+viikko 2:
+ tehtyjä tehtäviä 11/13 aikaa kului 5 tehdyt tehtävät: 1, 2, 3, 4, 6, 7, 8, 10, 11, 12, 13
+
+yhteensä: 26/30 tehtävää 25 tuntia
+
+ */
